75603be703d063f0440119b352dfc0800811228d
badhenke <henrikeriksson_90@hotmail.com>
2025-02-28 08:58:29 +0000

    Add force_video_length

--- start_production.sh
+++ start_production.sh
@@ -1,1 +1,1 @@
-SERVICE_ACCOUNT_KEY_PATH="/secrets/saltfish-434012-8c642217c8e8.json" uvicorn scripts.api:app --host 0.0.0.0 --port 8000 --workers 1
+SERVICE_ACCOUNT_KEY_PATH="/secrets/saltfish-434012-8c642217c8e8.json" nohup uvicorn scripts.api:app --host 0.0.0.0 --port 8000 --workers 1 > /home/henrikeriksson/latentsync_api.log 2>&1 &
